b741e35804205a1606b506c4d56a34005b2a94ea
Datacrowdgit <59723176+Datacrowdgit@users.noreply.github.com>
2022-03-01 16:50:58 -0600

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <div className="Header">
+      <p className="title">USCIS Tracker API</p>
+      <div className="hopts">
+          <p className="opt">
+              
+          </p>
+          <p className="opt">
+
+          </p>
+          <p className="opt">
+
+          </p>
+          <p className="opt">
+
+          </p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
