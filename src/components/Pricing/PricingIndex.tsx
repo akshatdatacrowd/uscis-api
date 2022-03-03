c6d861f02b056fdfd8716d329db8941e176ce306
Datacrowdgit <59723176+Datacrowdgit@users.noreply.github.com>
2022-03-03 01:33:23 -0600

    Pricing Page Completed

--- src/components/Pricing/PricingIndex.tsx
+++ src/components/Pricing/PricingIndex.tsx
@@ -1,3 +1,5 @@
+import { ImHome } from "react-icons/im";
+
 const PricingIndex = () => {
   return (
     <>
@@ -10,6 +12,76 @@ const PricingIndex = () => {
           <div className="MonYearBtnBtn" />
         </div>
         <p>Yearly (20% Discount)</p>
+      </div>
+      <div className="subscriptionOptionsBox">
+        <div className="suboptcontainer">
+          <div className="subopticon">
+            <ImHome size={40} />
+            <p>Free</p>
+          </div>
+          <div className="suboptPrice">
+            <p>$0</p>
+            <p>No Hidden Cost</p>
+          </div>
+          <div className="suboptSignUpBtn">
+            <p className="suboptSignUpBtnBtn">Sign Up</p>
+          </div>
+          <div className="suboptFeatures">
+            <p className="suboptFt">100 API Calls / mo</p>
+            <p className="suboptFt">100 API Calls / mo</p>
+          </div>
+        </div>
+        <div className="suboptcontainer">
+          <div className="subopticon">
+            <ImHome size={40} />
+            <p>Free</p>
+          </div>
+          <div className="suboptPrice">
+            <p>$0</p>
+            <p>No Hidden Cost</p>
+          </div>
+          <div className="suboptSignUpBtn">
+            <p className="suboptSignUpBtnBtn">Sign Up</p>
+          </div>
+          <div className="suboptFeatures">
+            <p className="suboptFt">100 API Calls / mo</p>
+            <p className="suboptFt">100 API Calls / mo</p>
+          </div>
+        </div>
+        <div className="suboptcontainer">
+          <div className="subopticon">
+            <ImHome size={40} />
+            <p>Free</p>
+          </div>
+          <div className="suboptPrice">
+            <p>$0</p>
+            <p>No Hidden Cost</p>
+          </div>
+          <div className="suboptSignUpBtn">
+            <p className="suboptSignUpBtnBtn">Sign Up</p>
+          </div>
+          <div className="suboptFeatures">
+            <p className="suboptFt">100 API Calls / mo</p>
+            <p className="suboptFt">100 API Calls / mo</p>
+          </div>
+        </div>
+        <div className="suboptcontainer">
+          <div className="subopticon">
+            <ImHome size={40} />
+            <p>Free</p>
+          </div>
+          <div className="suboptPrice">
+            <p>$0</p>
+            <p>No Hidden Cost</p>
+          </div>
+          <div className="suboptSignUpBtn">
+            <p className="suboptSignUpBtnBtn">Sign Up</p>
+          </div>
+          <div className="suboptFeatures">
+            <p className="suboptFt">100 API Calls / mo</p>
+            <p className="suboptFt">100 API Calls / mo</p>
+          </div>
+        </div>
       </div>
     </>
   );
