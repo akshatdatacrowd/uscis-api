0c25a5aa71e21d03362758a481f82b698e17f0e5
Datacrowdgit <59723176+Datacrowdgit@users.noreply.github.com>
2022-03-03 21:51:36 -0600

    added a play area

--- src/components/PlayArea/PlayArea.tsx
+++ src/components/PlayArea/PlayArea.tsx
@@ -1,12 +1,9 @@
 import { useState } from "react";
 
-
 const PlayArea = () => {
     const [Mouse, setMouse] = useState({top: 'inherit', left: 'inherit'})
-  
-  
   document.addEventListener("click", function(e){
-    setMouse({top: `${e.clientY - 25}px`, left: `${e.clientX - 25}px`})
+    setMouse({top: `${e.clientY - 50}px`, left: `${e.clientX - 50}px`})
     
   });
   return (
