730fe72342b7c52954b5d4d81e0563d75a525e17
Datacrowdgit <59723176+Datacrowdgit@users.noreply.github.com>
2022-03-04 17:12:57 -0600

    Added the sign up page

--- src/components/PlayArea/PlayArea.tsx
+++ src/components/PlayArea/PlayArea.tsx
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from "react";
 
 const PlayArea = () => {
-  const [Mouse, setMouse] = useState({ top: "inherit", left: "inherit" });
+  const [MouseX, setMouseX] = useState(0);
+  const [MouseY, setMouseY] = useState(0);
 
   useEffect(() => {
     const event = (e: MouseEvent) => {
-      setMouse({ top: `${e.clientY - 50}px`, left: `${e.clientX - 50}px` });
+      setMouseY(e.clientY - 50);
+      setMouseX(e.clientX - 50);
     };
     document.addEventListener("click", event);
     return () => document.removeEventListener("click", event);
   });
+
+  useEffect(() => {
+    const eventkd = (k: KeyboardEvent) => {
+      console.log(k)
+      if (k.key === "ArrowDown") {
+        setMouseY(MouseY + 50);
+      }
+      if (k.key === "ArrowUp") {
+        setMouseY(MouseY - 50);
+      }
+      if (k.key === "ArrowRight") {
+        setMouseX(MouseX + 50);
+      }
+      if (k.key === "ArrowLeft") {
+        setMouseX(MouseX - 50);
+      }
+      if (k.key === "r") {
+        setMouseX(0);
+        setMouseY(0);
+      }
+    };
+    document.addEventListener("keydown", eventkd);
+    return () => document.removeEventListener("keydown", eventkd);
+  });
+
   return (
     <>
       <div
         className="ball"
-        style={{ top: Mouse?.top, left: Mouse?.left }}
+        style={{
+          top: MouseY === 0 ? "inherit" : `${MouseY}px`,
+          left: MouseX === 0 ? "inherit" : `${MouseX}px`,
+        }}
       ></div>
     </>
   );
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,16 +1,51 @@
-
-
 const SignUp = () => {
   return (
     <div className="SignUpIndex">
-    <div className="SignUpFormContainer">
-        <form>
-            <label> Name </label>
-            <input type="text" />
-        </form>
+      <div className="SignUpFormContainer">
+        <div className="leftSUform">
+          <form>
+            <h1 id="SUHeading">Sign Up</h1>
+            <div className="IFSUContainer">
+              <input className="IFSU" type="text" placeholder="Full Name" />
+            </div>
+            <div className="IFSUContainer">
+              <input className="IFSU" type="email" placeholder="E-mail" />
+            </div>
+            <div className="IFSUContainer">
+              <input className="IFSU" type="password" placeholder="Password" />
+            </div>
+            <div className="IFSUContainer">
+              <input
+                className="IFSU"
+                type="password"
+                placeholder="Retype Password"
+              />
+            </div>
+            <div className="IFSUContainer">
+              <input className="SUsubmit" type="submit" value="Sign Me Up" />
+            </div>
+          </form>
+        </div>
+        <div className="rightSUform">
+          <div>
+            <button className="LoginSUBtns" id="fbsu">
+              Log in with Facebook
+            </button>
+          </div>
+          <div>
+            <button className="LoginSUBtns" id="twsu">
+              Log in with Twitter
+            </button>
+          </div>
+          <div>
+            <button className="LoginSUBtns" id="gosu">
+              Log in with Google
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
