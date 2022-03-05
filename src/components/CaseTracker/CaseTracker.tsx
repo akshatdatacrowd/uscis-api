3badb140ae2572d50a589155e2512c64baf48ffa
Datacrowdgit <59723176+Datacrowdgit@users.noreply.github.com>
2022-03-05 12:03:48 -0600

    Nailed the Tracker

--- src/components/CaseTracker/CaseTracker.tsx
+++ src/components/CaseTracker/CaseTracker.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { SyntheticEvent, useState } from "react";
 
 const CaseTracker = () => {
-  const [status, setStatus] = useState("");
-  const rpattern = '<div class="rows text-center">\n.+\n.+';
-  const onclick = async () => {
+  const [status, setStatus] = useState({
+    heading: "",
+    details: "",
+    CaseNumber: "",
+    LatestDate: "",
+    FormNumber: "",
+  });
+  const [CaseNumber, setCaseNumber] = useState("");
+  const rpattern1 = '<div class="rows text-center">\n.+\n.+';
+  const rpattern2 = ".+\n";
+  const rpatternFormNumber = "(I-\\d+)|(I-\\d+\\w)";
+  const rpatternDate = "\\w+\\s\\d+,\\s\\d+";
+  const searchCase = async (
+    e: SyntheticEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    e.preventDefault();
     const res = await fetch(
-      "https://api.allorigins.win/raw?url=https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=EAC2134250313"
+      `https://api.allorigins.win/raw?url=https://egov.uscis.gov/casestatus/mycasestatus.do?appReceiptNum=${CaseNumber}`
     );
     const data = await res.text();
-    const obj = data.match(rpattern);
+    const obj = data.match(rpattern1);
     if (obj !== null) {
       const preRawStatus = obj[0];
       const rawStatus = preRawStatus
@@ -18,20 +31,69 @@ const CaseTracker = () => {
           '<a href="https://www.uscis.gov/contactcenter" target="_blank">',
           ""
         )
+        .replace(
+          '<a href="https://egov.uscis.gov/e-Request/Intro.do" target="_blank">',
+          ""
+        )
         .replaceAll("</a>", "")
         .replace(
           '<a href="https://egov.uscis.gov/coa/displayCOAForm.do" target="_blank">',
           ""
         )
         .replace("</p>", "");
-      setStatus(rawStatus);
+      const rawHeading = rawStatus.match(rpattern2);
+      if (rawHeading !== null) {
+        const latestDate = rawStatus.match(rpatternDate);
+        const formNumber = rawStatus.match(rpatternFormNumber);
+        if (formNumber !== null && latestDate !== null) {
+          setStatus({
+            heading: rawHeading[0].replace("\n", ""),
+            details: rawStatus.replace(`${rawHeading[0]}`, ""),
+            CaseNumber: CaseNumber,
+            LatestDate: latestDate[0],
+            FormNumber: formNumber[0],
+          });
+        }
+      }
     }
   };
-
   return (
-    <div>
-      <button onClick={onclick}>Click Here</button>
-      <p>{status && status}</p>
+    <div className="CTIndex">
+      <h1 id="CTTitle">Case Tracker</h1>
+      <div className="FormCT">
+        <form>
+          <input
+            autoFocus={true}
+            id="CaseNumberInputCT"
+            type="text"
+            placeholder="13 digit Case Number"
+            value={CaseNumber}
+            onChange={(e) => setCaseNumber(e.target.value)}
+          />
+          <div>
+            <input
+              onClick={searchCase}
+              id="SubmitCT"
+              type="submit"
+              value="Search Case"
+            />
+          </div>
+        </form>
+      </div>
+      {status?.heading && (
+        <div className="StatusCT">
+          <h1 id="statusCTHeading">{status?.heading}</h1>
+          <p id="statusCTCaseNumber">Case Number : {status?.CaseNumber}</p>
+          <p id="statusCTCaseNumber">Date : {status?.LatestDate}</p>
+          <p id="statusCTCaseNumber">Form : {status?.FormNumber}</p>
+          <p id="statusCTDetails">{status?.details}</p>
+          <button id="SearchAnotherCaseCTBtn" onClick={() => {
+              setCaseNumber("");
+              }}>
+            Search Another Case
+          </button>
+        </div>
+      )}
     </div>
   );
 };
